7b4d98bdd839f5f5683d0729e7b5f6e372a6a249
José Astorga <jose.n.astorga.tobar@gmail.com>
2020-08-13 18:06:36 -0400

    [ADD] db connection retry :dog:

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 'use strict';
 
-const express = require('express');
+
+import express from 'express';
+import models, {sequelize} from './models/index.js';
+
+import router from './routes.js';
 
 const PORT = 4999;
 const HOST = '0.0.0.0';
 
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.use(express.json());
+app.use('/', router);
 
+
+const connectDb = async (retries = 5) => {
+  sequelize.sync().then( () => {
+    app.listen(PORT, HOST);
+    console.log(`Running on http://${HOST}:${PORT}`);
+  }).catch( () => {
+    
+    if(retries === 0){
+      console.error("Couldnt sync with DB - Goodbye");
+    }
+    console.log(`Couldnt sync :( \n Trying again in 5 seconds - Tries left: ${retries}`);
+    setTimeout( () => {connectDb(retries)}, 5000 )
+  })
+}
+
+connectDb();
