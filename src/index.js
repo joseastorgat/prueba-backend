cacfedbd4aaf77562ed78dfbd097c7aa1fbfa114
José Astorga <jose.n.astorga.tobar@gmail.com>
2020-08-14 16:08:38 -0400

    [FIX] connection retry

--- src/index.js
+++ src/index.js
@@ -13,17 +13,22 @@ app.use(express.json());
 app.use('/', authRouter);
 app.use('/', router);
 
+
 const connectDb = async (retries = 5) => {
   sequelize.sync().then( () => {
-    app.listen(PORT, HOST);
+    app.listen(process.env.PORT, process.env.HOST);
     console.log(`Running on http://${process.env.HOST}:${process.env.PORT}`);
-  }).catch( () => {
+  }).catch( (e) => {
     
-    if(retries === 0){
+    if(retries == 0){
       console.error("Couldnt sync with DB - Goodbye");
+      console.error(e);
     }
-    console.log(`Couldnt sync :( \n Trying again in 5 seconds - Tries left: ${retries}`);
-    setTimeout( () => {connectDb(retries)}, 5000 )
+    else{
+      
+      console.log(`Couldnt sync :( \n Trying again in 5 seconds - Tries left: ${retries}`);
+      setTimeout( () => {connectDb(retries-1)}, 5000 )
+  }
   })
 }
 
